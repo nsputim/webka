adc57650776faf02a91e3c4e2e00841920e9ff70
Evgeny <evgeny@putimtsev.ru>
2023-08-24 15:44:46 +0300

    start-3

--- app/src/main/java/com/smr/web/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/smr/web/ui/dashboard/DashboardViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "Авторизация по штрихкоду"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/smr/web/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/smr/web/ui/notifications/NotificationsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "Уведомления в разработке"
     }
     val text: LiveData<String> = _text
 }
